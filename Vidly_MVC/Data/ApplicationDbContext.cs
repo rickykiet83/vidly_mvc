7a9e8baf8c843243c629db25d81b26d12161ed0e
Kiet Pham <kietpham.dev@gmail.com>
2024-04-16 15:32:00 +1000

    Create customer entity migration

--- Vidly_MVC/Data/ApplicationDbContext.cs
+++ Vidly_MVC/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Vidly_MVC.Models;
 
 namespace Vidly_MVC.Data;
 
@@ -9,4 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+    public DbSet<Customer> Customers { get; set; }
 }
