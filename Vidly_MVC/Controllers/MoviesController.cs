16e6258cfeabd00d5f765f7befb2a8cbe7db513a
Kiet Pham <kietpham.dev@gmail.com>
2024-04-20 08:53:43 +1000

    Save Movie form data

--- Vidly_MVC/Controllers/MoviesController.cs
+++ Vidly_MVC/Controllers/MoviesController.cs
@@ -49,5 +49,45 @@ namespace Vidly_MVC.Controllers
             
             return View(movie);
         }
+        
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id.Equals(id));
+            if (movie == null)
+                return NotFound();
+            
+            var viewModel = new MovieFormViewModel(movie)
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View("MovieForm", viewModel);
+        }
+        
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+                _context.Movies.Add(movie);
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movies");
+        }
     }
 }
